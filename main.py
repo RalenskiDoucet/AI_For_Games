990198a2dc1f9c3debbff367dab0ea5c94a0a998
RalenskiDoucet <Ralenskid@gmail.com>
2018-01-30 09:05:34 -0600

    commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print ("Hello Word")
+def add(lhs,rhs):
+	sum=lhs + rhs
+	return sum
+def subtract(lhs,rhs):
+	remainder=lhs-rhs
+	return remainder
+def multiply(lhs,rhs):
+	product=lhs*rhs
+	return product
+def divide(lhs,rhs):
+	quotient=lhs/rhs
+	return quotient	
+A=raw_input("enter value")
+B=raw_input("enter value")
+operation=""
+if operation="+"add()
+elif operation="-"subtract()
+elif operation="*"multiply()
+elif operation="/"divide()
