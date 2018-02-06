a04d8202b82e0f241aca7aced3d2b5d52f990119
RalenskiDoucet <Ralenskid@gmail.com>
2018-02-06 11:22:24 -0600

    Finished Vector2 Class in Python
    Finished Vector2 Class in Python

--- Vector2/vector2.py
+++ Vector2/vector2.py
@@ -1,24 +1,30 @@
 """A class to a Vector2"""
 class Vector2(object):
-	def __init__(self, xpos,ypos):
+	def __init__(self, xpos, ypos):
+    		'''The inital decleration of the global class variables.'''
 		self.xpos = xpos
 		self.ypos = ypos
-
-	def __add__(self,other):
-		'''A function to add two Vect2s'''
-		new_Vector1=Vector2(self.xpos+ other.xpos)
-		new_Vector1=Vector2(self.ypos+ other.ypos)
-		return new_Vector1
-
-	def __sub__(self,other):
-		'''A Function to subtract two vect2s'''
-		new_Vector2=Vector2(self,xpos-other.xpos)
-		new_Vector2=Vector2(self,ypos-other.ypos)
-		return new_Vector2
-	
-	def multiplybyscalar(self,other):
-		'''A Function to multiply a vect2 by the scalor'''
-		new_Vector3=Vector2(self,xpos^2)
-		new_Vector3=Vector2(self,ypos^2)
-		return new_Vector3
-	
+	def __add__(self, other):
+			'''A function to add two Vect2s.'''
+			new_vector = Vector2(self.xpos + other.xpos,self.ypos + other.ypos)
+			return new_vector
+	def __sub__(self, other):
+			'''A Function to subtract two vect2s.'''
+			new_vector = Vector2(self.xpos - other.xpos, self.ypos - other.ypos)
+			return new_vector
+	def multiplybyscalar(self, other):
+			'''A Function to multiply a vect2 by the scalar.'''
+			new_vector =Vector2(self.xpos * other,self.ypos * other)
+			return new_vector
+	def dot(self, other):
+			'''A function that returns the dot product of two vectors.'''
+			new_vector = Vector2(self.xpos * other.xpos,self.ypos * other.ypos)
+			return new_vector
+	def magnitude(self):
+			'''A function that returns the magnitude of two vectors.'''
+			new_vector = Vector2(self.xpos * self.xpos, + self.ypos *self.ypos)
+			return new_vector
+	def __normalize__(self):
+			'''A function that normalizes a vector.'''
+			new_vector = Vector2(self.xpos / self.magnitude(), self.ypos / self.magnitude())
+			return new_vector
