2b7ccdd87850b67923012ee3bfe92bd008c5cedc
Ralenski Doucet <Ralenskid@gmail.com>
2018-02-07 09:53:33 -0600

    commit

--- AStar/NodeClass.py/AStar.py
+++ AStar/NodeClass.py/AStar.py
@@ -1,18 +1,10 @@
-import vector2.py
-'''A class for the node needed in Astar'''
-class node(object)
-    def __init__(self, gscore,score,fscore):
-        self.gscore=(cost+current)
-        self.hscore=10+(goal-current)
-        self.fscore=(gscore+hscore)
-    def __fetch_gscore__(self,gscore):
-        print gscore
-    def __fetch_hscore__(self,hscore):
-        print hscore
-    def __fetch_fscore__(self,fscore):
-        print fscore
-    def __parentnode__(self,xpos,ypos):
-        
-    def __postion__(self,):
-    
-    def __neighbors__(self,):
+ '''A class for the node needed in Astar.'''
+class Node(object)
+    def __init__(self, position):
+        self.postion = postion
+        self.is_transversible = False
+        self.parent_node = None
+        self.g_score = 0
+        self.h_score = 0
+        self.f_score = 0
+    def find_neighbors(self,position):
---
+++ AStar/NodeClass.py/Graph.py
@@ -0,0 +1,19 @@
+'''A class that generates a graph and nodes in the graph.'''
+from AStar import Node
+class Graph
+    def __init__(self, heigth, width):
+        self.nodes = []
+        self.heigth = heigth
+        self.width = width
+    def create_nodes(self):
+        #create a loop that will increment a value for a nodes x position
+            #create a loop that will increment a value for a nodes y position
+                #create a new node with the position value of x and y
+                #add the new node to the nodes list
+        for x in range (0,self.width):
+            for y in range (0,self.height):
+                new_node = node(Vector2(x,y))
+                self.nodes.append(new_node)
+        m_graph = __init__(heigth,width):
+        m_graph.create_nodes(1)
+
