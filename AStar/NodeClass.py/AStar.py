f761616fca748ccb57717081cce09dedc86d71b3
RalenskiDoucet <Ralenskid@gmail.com>
2018-02-19 11:02:22 -0600

    commit

--- AStar/NodeClass.py/AStar.py
+++ AStar/NodeClass.py/AStar.py
@@ -13,18 +13,27 @@ class Node(object):
         '''Compare each of the nodes position to see if it's position is a valid neighbor position'''
         '''add to neighbor list'''
         valid_positions = [] 
-        valid_positions.append (top_neighbor = current + <0, 1>
-        valid_positions.append (bottom_neigbor = current + <0, -1>)
-        valid_positions.append (left_neighbor = current + <-1, 0>)
-        valid_positions.append (right_neighbor = current + <1, 0>)
-    def calculate_gscore(self, g_score):
+        valid_positions.append (top_neighbor = current +  (<0, 1 > ))
+        valid_positions.append (bottom_neigbor = current +  (<0, -1 >)) 
+        valid_positions.append (left_neighbor = current +  < -1, 0 > 
+        valid_positions.append (right_neighbor = current +  < 1, 0 > 
+        valid_positions.append (north_east_neighbor = current +  < 1, +1 > 
+        valid_positions.append (north_west_neighbor = current +  < -1, +1 > 
+        valid_positions.append (south_east_neighbor = current +  < 1, -1 > 
+        valid_positions.append (south_west_neighbor = current +  < -1, -1 > 
+        return valid_positions[]
+    def calculate_gscore(self, g_score, position):
         '''Loop through the graph to find current node's position'''
         '''Find movement cost'''
         '''Add current node to movement cost'''
+        '''Change current node to a neighbor node and find its position.'''
+        '''Add the new current node movement cost.'''
+        '''Do to all transversible nodes.'''
         movement_cost=10
-        current_node=raw_input("")
-        goal_node=raw_input("")
-        g_score=movement_cost+current_node
+        current_node.find_neighbors():
+        goal_node.position = vector2 (raw_input("<xpos , ypos>"))
+        
+
         print g_score
     def calcuate_hscore(self,postion,g_score):
         '''Loop through graph find the number of nodes between current and goal then number of nodes times 10.'''
---
+++ morining bellwork/Bellwork.py
@@ -0,0 +1,16 @@
+'''In python create an application that will find all items in a list
+that are less than the value the user specifies. These items should
+be added to new list and then the list should be printed to the
+screen.'''
+class mList(object):
+    def __init__(self, m_list):
+        self.m_list = m_list
+    def find_items(self, m_list):
+        for x in range(10, self.m_list):
+            new_list=mList(m_list())
+            self.m_list.append(new_list)
+            if x < m_list[raw_input("")]
+            print "items_found"new_list
+            elif x > m_list[raw_input("")]:
+            print "no_item_found"
+    mList = __init__(self, m_list):
