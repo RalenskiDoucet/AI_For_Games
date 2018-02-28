016b2ecc97ebdbccbae1e5691f031001d7e5fe65
RalenskiDoucet <Ralenskid@gmail.com>
2018-02-28 09:20:52 -0600

    commit

--- AStar/Astar Project/Graph.py
+++ AStar/Astar Project/Graph.py
@@ -15,34 +15,41 @@ class Graph(object):
                 self.nodes.append(n)
 class Node(object):
     def __init__(self, guid, pos):
-        self.__guid = guid
+        self.guid = guid
         self.position = pos
+        self.g_score
+        self.h_score
+        self.f_score
     def find_neighbors(self,graph):
         valid_neighbors = []
-        valid_neighbors.append(guid - self.dims)'''top_neighbor'''
-        valid_neighbors.append(guid + self.dims)'''bot_neighbor'''
-        valid_neighbors.append(guid - 1)'''left_neighbor'''
-        valid_neighbors.append(guid + 1)'''right_neighbor'''
-        valid_neighbors.append(guid - self.dims - 1)'''top_left'''
-        valid_neighbors.append(guid - self.dims + 1)'''top_right'''
-        valid_neighbors.append(guid + self.dims - 1)'''bot_left'''
-        valid_neighbors.append(guid + self.dims + 1)'''bot_right'''
-    def find_gscore(self,Node):
+        valid_neighbors.append(guid(1) - self.dims)#top_neighbor
+        valid_neighbors.append(guid(2) + self.dims)#bot_neighbor
+        valid_neighbors.append(guid(3) - 1)#left_neighbor
+        valid_neighbors.append(guid(4) + 1)#right_neighbor
+        valid_neighbors.append(guid(5) - self.dims - 1)#top_left
+        valid_neighbors.append(guid(6) - self.dims + 1)#top_right
+        valid_neighbors.append(guid(7) + self.dims - 1)#bot_left
+        valid_neighbors.append(guid(8) + self.dims + 1)#bot_right
+    def find_gscore(self,Node,g_score):
         if ((self.postion.xpos == Node.position.xpos and self.postion.ypos != Node.position.ypos) or
         (self.postion.xpos != Node.position.xpos and self.postion.ypos == Node.position.ypos)):
-        self.g_score = Node.g_score + 10
+            self.g_score = Node.g_score + 10
         else:
          self.g_score = Node.g_score + 14
          return g_score
-    def find_hscore(self,Node,other):
+    def find_hscore(self,Node,h_score):
         h_score=((self.xpos - other.xpos and self.ypos - other.ypos * 10))
         return h_score 
-    def find_fscore(self,Node,other):
+    def find_fscore(self,Node,f_score):
         f_score = g_score + h_score
         return f_score
 def main():
     g = Graph(10)
     a = Node(100)
+    n.find_neighbors(15)
+    jk.find_gscore()
+    jk.find_hscore()
+    jk.find_fscore()
     
         
 
---
+++ AStar/Morning Assignments and Class Assignments/MorningAssignmentfeb28.py
@@ -0,0 +1,39 @@
+#Write a program to ask the user to enter a string of characters and find its length.  
+#Then provide the following information about this string:
+#a.
+#Print the uppercase letters (if any) in the string
+#b.
+#Print every 2nd character in the string
+#c.
+#Print the string with all vowels replaced by an underscore
+#and count/print the number of vowels
+def __init__(self,mstring):
+        self.m_string=mstring
+        self.length
+def string_length(self,mstring):
+    for i in range (0-50):
+        print i
+def print_every_2nd_char(self,mstring):
+    for (mstring[i], i is 1, iter++):
+        iter+2
+        print i
+def replace_vowels(self,mstring):
+    vowels=[]
+    vowels.append(a)
+    vowels.append(e)
+    vowels.append(i)
+    vowels.append(o)
+    vowels.append(u)
+    for(mstring[i] i = vowels):
+        vowels[i]=_
+        return _
+
+def main():
+    
+    mystring = "Ralenski"
+    mystring.count
+    mystring.isupper
+    mystring.count
+
+
+main()
